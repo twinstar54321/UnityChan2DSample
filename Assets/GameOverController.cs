f9190e6fda0670d9fa9b9a8d53ab9c766503ae84
twinstar <twinstar564219@gmail.com>
2018-08-08 01:53:35 +0900

    first commit

---
+++ Assets/GameOverController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour {
+
+    [SceneName]
+    public string nextLevel;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(GetComponent<AudioSource>().clip.length + 1);
+
+        SceneManager.LoadScene(nextLevel);
+    }
+}
---
+++ Assets/UnityChan2D/Demo/Scripts/UnityChanVoice.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(UnityChan2DController), typeof(AudioSource))]
+public class UnityChanVoice : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip damageVoice;
+
+    [SerializeField]
+    private AudioClip[] jumpVoices;
+
+    [SerializeField]
+    private AudioClip[] clearVoice;
+
+    [SerializeField]
+    private AudioClip timeOverVoice;
+
+    [SerializeField]
+    private AudioClip GameOverVoice;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnDamage()
+    {
+        PlayVoice(damageVoice);
+    }
+
+    void Jump()
+    {
+        int i = Random.Range(0, jumpVoices.Length);
+        PlayVoice(jumpVoices[i]);
+    }
+
+    void Clear()
+    {
+        int i = Random.Range(0, clearVoice.Length);
+        PlayVoice(clearVoice[i]);
+    }
+
+    void TimeOver()
+    {
+        PlayVoice(timeOverVoice);
+    }
+
+    void GameOver()
+    {
+        PlayVoice(GameOverVoice);
+    }
+
+    void PlayVoice(AudioClip voice)
+    {
+        audioSource.Stop();
+        audioSource.PlayOneShot(voice);
+    }
+}
